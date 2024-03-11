2f6849cfc312d33dc96099fbd250ff6232e9d85d
Lívia Amaral <125597286+Liviaamaralsales@users.noreply.github.com>
2024-03-10 22:31:34 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,29 @@
+openMenu.addEventListener('click', () => {
+
+    menubar.style.display = "flex"
+    
+    menu.style.right = (menu.offsetWidth * -1) + 'px'
+
+    setTimeout(() => {
+
+        menu.style.opacity = '1'
+
+        menu.style.right = "0"
+
+        openMenu.style.display = 'none'
+    }, 10);
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.style.opacity = '0'
+
+    menu.style.right = (menu.offsetWidth * -1) + 'px'
+
+    setTimeout(()=> {
+
+		menu.removeAttribute('style')
+
+		openMenu.removeAttribute('style')
+        
+	}, 200);
+})
